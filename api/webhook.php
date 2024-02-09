a20d1bcd5ccffb7624b15867263fe41924e687fe
KLafosne <109802795+KLafosne@users.noreply.github.com>
2024-02-09 23:20:22 +0800

    Update webhook.php

--- api/webhook.php
+++ api/webhook.php
@@ -8,5 +8,6 @@ $message = $data['message']['text'] ?? '';
 if ($message === '/token') {
     $chat_id = $data['message']['chat']['id'];
     $bot->sendMessage(['text' => $bot->encryption($chat_id), 'chat_id' => $chat_id]);
-}
+}else if ($message === '/help') {
+    return '这是一个帮助信息！';
 echo json_encode(['code' => 200, 'message' => 'success']);
